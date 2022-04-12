2ca2352189d7708ed4526da2ad50c3ad12bd2ff7
Andrew Lalis <andrewlalisofficial@gmail.com>
2022-04-12 11:31:26 +0200

    Added null checks.

--- src/main/java/nl/andrewl/email_indexer/gen/transform/AddressCleaner.java
+++ src/main/java/nl/andrewl/email_indexer/gen/transform/AddressCleaner.java
@@ -10,10 +10,10 @@ public class AddressCleaner implements Consumer<Email> {
         if (email.messageId.startsWith("<") && email.messageId.endsWith(">")) {
             email.messageId = email.messageId.substring(1, email.messageId.length() - 1);
         }
-        if (email.inReplyTo.startsWith("<") && email.inReplyTo.endsWith(">")) {
+        if (email.inReplyTo != null && email.inReplyTo.startsWith("<") && email.inReplyTo.endsWith(">")) {
             email.inReplyTo = email.inReplyTo.substring(1, email.inReplyTo.length() - 1);
         }
-        if (email.sentFrom.startsWith("<") && email.sentFrom.endsWith(">")) {
+        if (email.sentFrom != null && email.sentFrom.startsWith("<") && email.sentFrom.endsWith(">")) {
             email.sentFrom = email.sentFrom.substring(1, email.sentFrom.length() - 1);
         }
     }
