a305e6cfcd8dda64336b26b1ecbfa85b67f38796
Andrew Lalis <andrewlalisofficial@gmail.com>
2022-04-14 17:21:57 +0200

    Added javadox.

--- src/main/java/nl/andrewl/email_indexer/data/EmailIndexSearcher.java
+++ src/main/java/nl/andrewl/email_indexer/data/EmailIndexSearcher.java
@@ -20,6 +20,13 @@ import java.util.concurrent.ForkJoinPool;
  * indexes.
  */
 public class EmailIndexSearcher {
+	/**
+	 * Searches the dataset asynchronously using the given query, and returns
+	 * an ordered list of root email ids for threads containing relevant emails.
+	 * @param dataset The dataset to search.
+	 * @param queryString The query to use.
+	 * @return A future that completes when the search is done.
+	 */
 	public CompletableFuture<List<String>> searchAsync(EmailDataset dataset, String queryString) {
 		CompletableFuture<List<String>> cf = new CompletableFuture<>();
 		ForkJoinPool.commonPool().submit(() -> {
@@ -32,6 +39,15 @@ public class EmailIndexSearcher {
 		return cf;
 	}
 
+	/**
+	 * Searches the dataset using the given query, and returns an ordered list
+	 * of root email ids for threads containing relevant emails.
+	 * @param dataset The dataset to search.
+	 * @param queryString The query to use.
+	 * @return A list of root email ids.
+	 * @throws IOException If an error occurs while opening the indexes.
+	 * @throws ParseException If the query is invalid.
+	 */
 	public List<String> search(EmailDataset dataset, String queryString) throws IOException, ParseException {
 		MultiFieldQueryParser queryParser = new MultiFieldQueryParser(
 				new String[]{"subject", "body"},
