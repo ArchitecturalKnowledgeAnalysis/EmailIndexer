505efdc360c0c6c5f75bdff53683f9a754ca27b3
Andrew Lalis <andrewlalisofficial@gmail.com>
2022-06-13 07:39:32 +0200

    Bumped version, fixed javadoc.

--- src/main/java/nl/andrewl/email_indexer/data/search/filter/ParentIdFilter.java
+++ src/main/java/nl/andrewl/email_indexer/data/search/filter/ParentIdFilter.java
@@ -5,6 +5,7 @@ import nl.andrewl.email_indexer.data.search.SearchFilter;
 /**
  * Filter that limits search results to only those emails whose PARENT_ID
  * matches the given non-null value.
+ * @param parentId The id of the parent email.
  */
 public record ParentIdFilter(long parentId) implements SearchFilter {
 	@Override
